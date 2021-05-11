65dde8aa9c1a234570ce52d9203772241c9ef9ef
g_koitan <tk_kakutasu@yahoo.co.jp>
2021-05-12 03:59:52 +0900

    かわずたんの色違いとアウトラインを追加

---
+++ Assets/Scripts/Koitan/SpriteTest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+public class SpriteTest : MonoBehaviour
+{
+    SpriteRenderer sr;
+    SpriteSkin ss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryGetComponent(out sr);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sr.sprite.ge
+        //ss.rootBone = 
+    }
+}
