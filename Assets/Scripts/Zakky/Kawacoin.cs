3dd788e1900ed23169b9bd64751f6928fd5d1888
Zakky03 <zakky0317@icloud.com>
2021-06-06 17:37:31 +0900

    キャラの色変えした

--- Assets/Scripts/Zakky/Kawacoin.cs
+++ Assets/Scripts/Zakky/Kawacoin.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 using Koitan;
 
 public class Kawacoin : MonoBehaviour
@@ -10,33 +11,49 @@ public class Kawacoin : MonoBehaviour
     int id;
     [SerializeField]
     CursorHand cursorHand;
+    //[SerializeField]
+    //GameObject Player;
+    [SerializeField]
+    PlayerController playerController;
 
     public int ID { get; private set; }
+    //List<Collider2D> colList;
 
     void Start()
     {
         ID = id;
+        //colList = new List<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IsCollision();
+
+        if (cursorHand.Havecoin) transform.DOScale(1.3f, 0.1f);
+        else transform.DOScale(1f, 0.1f);
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    void IsCollision()
     {
-        if (collision.tag == "SelectCharacter")
-        {
-            if (!cursorHand.Havecoin)
-            {
-                BattleSetting.charaColorIndexes[ID] = 0;
-                //collision.GetComponent<SelectCharacter>()
-                //ââèo
-            }
-            else
-            {
+        Collider2D[] collisions = Physics2D.OverlapCircleAll(transform.position, GetComponent<CircleCollider2D>().radius);
 
+        foreach (Collider2D col in collisions)
+        {
+            if (col.tag == "CharaIcon")
+            {
+                if (!cursorHand.Havecoin)
+                {
+                    //BattleSetting.playerIndexes[ID] = col.GetComponent<CharaIcon>().CharaID;
+                    playerController.ChangeColor(col.GetComponent<CharaIcon>().CharaID, col.GetComponent<CharaIcon>().CharaID);
+                    Debug.Log("Selected");
+                    //collision.GetComponent<SelectCharacter>();
+                    //ââèo
+                }
+                else
+                {
+
+                }
             }
         }
     }
